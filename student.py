29c9d8fefd21fd23958c695948b4679138f984f5
WinslettC <winslettc@gilmour.org>
2017-10-02 14:47:48 -0400

    edit dance function

--- student.py
+++ student.py
@@ -74,7 +74,7 @@ class Piggy(pigo.Pigo):
     def to_the_left(self):
         """subroutine of dance method/ turns left and then pulses four times"""
         for x in range(1):
-            self.encL(28)
+            self.encL(32)
             for x in range(4):
                 self.encF(10)
                 self.stop()
