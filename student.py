08576e97f8e9630d92136d6996c590155f698928
WinslettC <winslettc@gilmour.org>
2017-10-13 15:04:48 -0400

    fixed indent error

--- student.py
+++ student.py
@@ -195,7 +195,7 @@ class Piggy(pigo.Pigo):
 
     def cruise(self):
         """Drives robot forward while the coast is clear"""
-        self.encF(15)
+        self.fwd()
         print("DRIVING")
         while self.dist() > self.SAFE_STOP_DIST:
             time.sleep(.5)
