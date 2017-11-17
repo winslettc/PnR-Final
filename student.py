5bfad9fb11349657f2dc4f433f80e4e53b54f4ce
WinslettC <winslettc@gilmour.org>
2017-11-17 14:46:09 -0500

    edit nav method

--- student.py
+++ student.py
@@ -287,13 +287,12 @@ class Piggy(pigo.Pigo):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         while True:
             self.datetime()
-            for x in range(10):
-                self.smart_turn()
-                self.servo(self.MIDPOINT)
-                print("\n----Setting Midpoint----\n")
-                self.cruise()
-                self.restore_heading()
-                time.sleep(.5)
+            self.smart_turn()
+            self.servo(self.MIDPOINT)
+            print("\n----Setting Midpoint----\n")
+            self.cruise()
+            self.restore_heading()
+
 
     def alternate_method(self):
         """Backs up robot when there is no free space and chooses an alternate route with free space"""
