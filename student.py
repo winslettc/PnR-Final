b6abd9b618da8e36a5d1f2b236f7efb4f1e04172
WinslettC <winslettc@gilmour.org>
2017-11-20 15:04:07 -0500

    edit back up method

--- student.py
+++ student.py
@@ -286,13 +286,13 @@ class Piggy(pigo.Pigo):
         """Backs up and turns left simultaneously"""
         self.set_speed(60, 200)
         for x in range(2):
-            self.encB(9)
+            self.encB(8)
 
     def back_turn_right(self):
         """Backs up and turns right"""
         self.set_speed(200,60)
         for x in range(2):
-            self.encB(9)
+            self.encB(8)
 
     def nav(self):
         """auto pilots and attempts to maintain original heading"""
